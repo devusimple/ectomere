6b597ca5890222b550ca35d1b302ede47feb3804
devusimple <nahidhasan195446@gmail.com>
2025-05-07 13:33:45 +0600

    update

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello from ectomere server!");
 });
 
+app.use("/api/v1", (req: Request, res: Response) => {
+  res.json({ message: "Hello from ectomere server!" });
+});
+
 app.listen(3000, () => {
   console.info("ectomere server running!");
 });
